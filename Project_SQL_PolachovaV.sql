60faa3914e09d37a3d0cb0fa695c2fa0372204f0
VeronikaPolachova <veronika.cunderlova@gmail.com>
2024-12-03 16:59:36 +0100

    avg payroll quarters, podminka industry branch code

--- Project_SQL_PolachovaV.sql
+++ Project_SQL_PolachovaV.sql
@@ -48,7 +48,7 @@ ON cp.region_code = cr.code
 -- JOIN selectu A a B a economies
 -- tabulka 1
 
-CREATE TABLE t_veronika_polachova_project_SQL_primary_final AS 
+CREATE OR REPLACE TABLE t_veronika_polachova_project_SQL_primary_final AS 
 SELECT
 	A.*,
 	B.*, 
@@ -74,14 +74,14 @@ FROM (
 	LEFT JOIN (
 			SELECT 
 			cp.payroll_year, 
-			cp.payroll_quarter, 
+			-- cp.payroll_quarter, 
 			cp.calculation_code, 
 			cpc.name AS calculation_code_name,
 			cp.industry_branch_code, 
 			cpib.name AS industry_branch_name,
 			cpvt.code AS value_type_code,
 			cpvt.name AS value_type,
-			cp.value AS value_payroll,
+			AVG(cp.value) AS value_payroll,
 			cp.unit_code AS unit_code,
 			cpu.name AS unit
 		FROM czechia_payroll as cp
@@ -93,6 +93,7 @@ FROM (
 			ON cp.unit_code = cpu.code 
 		LEFT JOIN czechia_payroll_value_type as cpvt 
 			ON cp.value_type_code = cpvt.code
+		GROUP BY cp.payroll_year, cp.calculation_code, cpc.name, cp.industry_branch_code, cpib.name, cpvt.code, cpvt.name, cp.unit_code, cpu.name
 			) AS B
 	ON A.year = B.payroll_year
 LEFT JOIN economies e 
@@ -101,6 +102,8 @@ LEFT JOIN economies e
 WHERE A.region_code IS NULL 
 AND B.value_payroll IS NOT NULL
 AND country = 'Czech Republic'
+AND B.calculation_code = 200
+AND B.industry_branch_code IS NOT NULL
 ;
 
 SELECT *
