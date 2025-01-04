3c4a1460def7668706c2044347be86c73a1b4763
VeronikaPolachova <veronika.cunderlova@gmail.com>
2025-01-04 22:49:40 +0100

    interannual decrease

--- otazka1.sql
+++ otazka1.sql
@@ -8,7 +8,7 @@ WITH cte_salary_history AS (
 		industry_branch_name,
 		value_payroll AS salary,
 		LAG (AVG (value_payroll)) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year) AS previous_year_salary,
-		ROUND (((AVG(value_payroll) - LAG (AVG (value_payroll)) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year))/ LAG (AVG (value_payroll)) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year))*100, 2) AS salary_annual_growth -- mezirocni narust mezd v procentech
+		ROUND (((AVG(value_payroll) - LAG (AVG (value_payroll)) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year))/ LAG (AVG (value_payroll)) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year))*100, 2) AS salary_interannual_growth -- mezirocni narust mezd v procentech
 	FROM t_veronika_polachova_project_SQL_primary_final tvppspf
 	WHERE value_type_code = 5958
 	GROUP BY payroll_year, industry_branch_name, value_payroll 
@@ -16,22 +16,68 @@ WITH cte_salary_history AS (
 	)
 SELECT *,
 	CASE 
-			WHEN salary_annual_growth > 0 THEN 'annual_increase'
-			WHEN salary_annual_growth < 0 THEN 'annual_decrease'
-			WHEN salary_annual_growth = 0 THEN 'annual_stagnation'
+			WHEN salary_interannual_growth > 0 THEN 'interannual_increase'
+			WHEN salary_interannual_growth < 0 THEN 'interannual_decrease'
+			WHEN salary_interannual_growth = 0 THEN 'interannual_stagnation'
 			ELSE NULL
-		END AS flag_annual_salary_growth,
+		END AS flag_interannual_salary_growth,
 		CASE 
-			WHEN salary_annual_growth IS NOT NULL THEN ROUND(AVG(salary_annual_growth) OVER (PARTITION BY industry_branch_name),2)
+			WHEN salary_interannual_growth IS NOT NULL THEN ROUND(AVG(salary_interannual_growth) OVER (PARTITION BY industry_branch_name),2)
 			ELSE NULL
 		END AS overall_growth_per_industry, -- celkovy prumerny rust mezd v kazdem odetvi za celkove obdobi 
 		CASE
-			WHEN AVG(salary_annual_growth) OVER (PARTITION BY industry_branch_name) > 0 THEN 'overall_increase'
-			WHEN AVG(salary_annual_growth) OVER (PARTITION BY industry_branch_name) < 0 THEN 'overall_decrease'
-			WHEN AVG(salary_annual_growth) OVER (PARTITION BY industry_branch_name) = 0 THEN 'overall_stagnation'
+			WHEN AVG(salary_interannual_growth) OVER (PARTITION BY industry_branch_name) > 0 THEN 'overall_increase'
+			WHEN AVG(salary_interannual_growth) OVER (PARTITION BY industry_branch_name) < 0 THEN 'overall_decrease'
+			WHEN AVG(salary_interannual_growth) OVER (PARTITION BY industry_branch_name) = 0 THEN 'overall_stagnation'
 			ELSE NULL
 		END AS flag_overall_growth_per_industry
 FROM cte_salary_history
-ORDER BY industry_branch_name, payroll_year
+ORDER BY industry_branch_name, payroll_year 
 ;
 
+-- konkretneji
+WITH cte_overall_salary_statistics AS (
+	WITH cte_salary_history AS (
+		SELECT 
+			payroll_year,
+			industry_branch_name,
+			value_payroll AS salary,
+			LAG (AVG (value_payroll)) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year) AS previous_year_salary,
+			ROUND (((AVG(value_payroll) - LAG (AVG (value_payroll)) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year))/ LAG (AVG (value_payroll)) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year))*100, 2) AS salary_interannual_growth -- mezirocni narust mezd v procentech
+		FROM t_veronika_polachova_project_SQL_primary_final tvppspf
+		WHERE value_type_code = 5958
+		GROUP BY payroll_year, industry_branch_name, value_payroll 
+		ORDER BY industry_branch_name, payroll_year 
+		)
+	SELECT *,
+		CASE 
+				WHEN salary_interannual_growth > 0 THEN 'interannual_increase'
+				WHEN salary_interannual_growth < 0 THEN 'interannual_decrease'
+				WHEN salary_interannual_growth = 0 THEN 'interannual_stagnation'
+				ELSE NULL
+			END AS flag_interannual_salary_growth,
+			CASE 
+				WHEN salary_interannual_growth IS NOT NULL THEN ROUND(AVG(salary_interannual_growth) OVER (PARTITION BY industry_branch_name),2)
+				ELSE NULL
+			END AS overall_growth_per_industry, -- celkovy prumerny rust mezd v kazdem odetvi za celkove obdobi 
+			CASE
+				WHEN AVG(salary_interannual_growth) OVER (PARTITION BY industry_branch_name) > 0 THEN 'overall_increase'
+				WHEN AVG(salary_interannual_growth) OVER (PARTITION BY industry_branch_name) < 0 THEN 'overall_decrease'
+				WHEN AVG(salary_interannual_growth) OVER (PARTITION BY industry_branch_name) = 0 THEN 'overall_stagnation'
+				ELSE NULL
+			END AS flag_overall_growth_per_industry
+	FROM cte_salary_history
+	ORDER BY industry_branch_name, payroll_year 
+	)
+SELECT
+	payroll_year, 
+	industry_branch_name, 
+	salary, 
+	previous_year_salary, 
+	flag_interannual_salary_growth, 
+	flag_overall_growth_per_industry
+FROM cte_overall_salary_statistics
+WHERE 1=1
+AND flag_interannual_salary_growth IN ('interannual_decrease', 'interannual_stagnation') -- pokud chci konkretní rok a odvetvi, kdy mezirovne nedoslo k rustu platu
+-- AND flag_overall_growth_per_industry = 'overall_decrease' -- pokud chci videt, v jakych odvetvich doslo k celkovemu poklesu mezd za dane casove obdobi
+;		
