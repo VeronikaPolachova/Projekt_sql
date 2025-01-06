dd353cfab1bb2b501dc84eb2abe21474c73923cd
VeronikaPolachova <veronika.cunderlova@gmail.com>
2025-01-06 16:39:56 +0100

    previous year price, perc. growth

--- otazka3.sql
+++ otazka3.sql
@@ -3,10 +3,12 @@
 
 SELECT 
 	year, 
-	ROUND (AVG (value), 2) AS price,
 	category,
 	price_value, 
-	price_unit
+	price_unit,
+	ROUND (AVG (value), 2) AS price_in_CZK,
+	ROUND(LAG (AVG (value)) OVER (PARTITION BY category ORDER BY year), 2) AS previous_year_price_in_CZK, 
+    ROUND (((AVG (value) - LAG (AVG (value)) OVER (PARTITION BY category ORDER BY year))/(LAG (AVG (value)) OVER (PARTITION BY category ORDER BY year)))*100, 2) AS price_percentage_growth
 FROM t_veronika_polachova_project_SQL_primary_final tvppspf 
 GROUP BY year, category, price_value, price_unit 
 ORDER BY category, year
