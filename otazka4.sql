bf00aa2f5e682c3294c0b4a099028c90f1c0b250
VeronikaPolachova <veronika.cunderlova@gmail.com>
2025-01-07 11:13:15 +0100

    avg_salary nehlede na pocty zamestnancu, procent.rust mezd

--- otazka4.sql
+++ otazka4.sql
@@ -1,30 +1,20 @@
 -- OTÁZKA 4
 -- Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
 
-SELECT *
-FROM (
-	SELECT
-		payroll_year,
-		industry_branch_code,
-		industry_branch_name,
-		ROUND(AVG (value_payroll)) as avg_salary
-	FROM t_veronika_polachova_project_SQL_primary_final tvppspf 
-	WHERE value_type_code = 5958
-	AND industry_branch_code IS NOT NULL
-	GROUP BY payroll_year, industry_branch_name, industry_branch_code
-	ORDER BY industry_branch_code, `year`
-	) AS A
-LEFT JOIN (
-	SELECT 
-			payroll_year, 
-			industry_branch_name, 
-			AVG (value_payroll) AS avg_count, 
-			unit
-	FROM t_veronika_polachova_project_SQL_primary_final tvppspf 
-	WHERE value_type_code = 316
-	AND industry_branch_name IS NOT NULL
-	GROUP BY payroll_year, industry_branch_name, unit 
-	) AS B
-ON A.payroll_year = B.payroll_year
-AND A.industry_branch_name = B.industry_branch_name
+CREATE VIEW v_VPolachova_o4 AS
+SELECT 
+	payroll_year, 
+	ROUND(avg(value_payroll)) AS avg_salary,
+	unit
+FROM t_veronika_polachova_project_SQL_primary_final tvppspf 
+WHERE value_type_code = 5958
+GROUP BY payroll_year, unit
+;
+
+SELECT 
+	payroll_year, 
+	avg_salary,
+	LAG (avg_salary) OVER (order by avg_salary) AS previous_year_salary,
+	ROUND ((avg_salary - (LAG (avg_salary) OVER (order by avg_salary)))/(LAG (avg_salary) OVER (order by avg_salary))*100, 2) AS percentage_salary_growth
+FROM v_vpolachova_o4 vvo 
 ;
