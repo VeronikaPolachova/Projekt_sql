474ffd3426f6b240b38768c1a08984c4bb1dda3c
VeronikaPolachova <veronika.cunderlova@gmail.com>
2025-01-09 23:16:14 +0100

    zpusob bez views

--- otazka4.sql
+++ otazka4.sql
@@ -27,7 +27,7 @@ FROM
 	(SELECT 
 		payroll_year, 
 		avg_salary_in_CZK,
-		LAG (avg_salary_in_CZK) OVER (order by avg_salary_in_CZK) AS previous_year_salary,
+		LAG (avg_salary_in_CZK) OVER (order by payroll_year) AS previous_year_salary,
 		ROUND ((avg_salary_in_CZK - (LAG (avg_salary_in_CZK) OVER (order by avg_salary_in_CZK)))/(LAG (avg_salary_in_CZK) OVER (order by avg_salary_in_CZK))*100, 2) AS percentage_salary_growth
 	FROM v_vpolachova_o4 vvo
 	) AS A
@@ -35,9 +35,25 @@ LEFT JOIN
 	(SELECT 
 		year,
 		avg_price_in_CZK,
-		LAG (avg_price_in_CZK) OVER (order by avg_price_in_CZK) AS previous_year_price,
-		ROUND ((avg_price_in_CZK - (LAG (avg_price_in_CZK) OVER (order by avg_price_in_CZK)))/(LAG (avg_price_in_CZK) OVER (order by avg_price_in_CZK))*100, 2) AS percentage_price_growth
+		LAG (avg_price_in_CZK) OVER (order by year) AS previous_year_price,
+		ROUND ((avg_price_in_CZK - (LAG (avg_price_in_CZK) OVER (order by year)))/(LAG (avg_price_in_CZK) OVER (order by year))*100, 2) AS percentage_price_growth
 	FROM v_vpolachova_o4_2 vvo2 
 	) AS B
 ON A.payroll_year = B.year
 ;
+
+-- jiny zpusob, bez views, kompaktneji
+
+SELECT 
+	payroll_year,
+	ROUND (AVG (value_payroll)) AS avg_salary_in_CZK,
+	ROUND (LAG (AVG (value_payroll)) OVER (order by payroll_year)) AS previous_year_salary,
+	ROUND (AVG (value), 1) AS avg_price_in_CZK,
+	ROUND (LAG (AVG (value)) OVER (order by payroll_year), 1) AS previous_year_price
+FROM t_veronika_polachova_project_SQL_primary_final tvppspf 
+WHERE value_type_code = 5958
+GROUP BY payroll_year 
+;
+
+
+
