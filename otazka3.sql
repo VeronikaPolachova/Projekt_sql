7ae677a81225e9a2c323ee56ae54558398c58f54
VeronikaPolachova <veronika.cunderlova@gmail.com>
2025-01-11 18:27:07 +0100

    minus value_type_code a minus concat

--- otazka3.sql
+++ otazka3.sql
@@ -4,7 +4,9 @@
 WITH cte_food_price_history AS (
 	SELECT 
 		year, 
-		CONCAT (category,' ', price_value, ' ', price_unit) AS category_and_unit, 
+		category, 
+		price_value, 
+		price_unit,
 		ROUND (AVG (value), 2) AS price_in_CZK,
 		ROUND(LAG (AVG (value)) OVER (PARTITION BY category ORDER BY year), 2) AS previous_year_price_in_CZK, 
 	    ROUND (((AVG (value) - LAG (AVG (value)) OVER (PARTITION BY category ORDER BY year))/(LAG (AVG (value)) OVER (PARTITION BY category ORDER BY year)))*100, 2) AS price_percentage_growth
@@ -13,10 +15,13 @@ WITH cte_food_price_history AS (
 	ORDER BY category, year
 	)
 SELECT 
-	category_and_unit,
+	category,
+	price_value,
+	price_unit,
 	ROUND (AVG (price_percentage_growth), 2) AS avg_percentage_growth
 FROM cte_food_price_history 
-GROUP BY category_and_unit
+GROUP BY category, price_value, price_unit
 ORDER BY avg_percentage_growth
+;
 ;
 
