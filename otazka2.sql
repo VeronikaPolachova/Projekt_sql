e08ad0577674c20253cf9d89fd9805e8397ee476
VeronikaPolachova <veronika.cunderlova@gmail.com>
2025-01-11 18:18:28 +0100

    minus value_type_code a minus concaty

--- otazka2.sql
+++ otazka2.sql
@@ -5,13 +5,15 @@
 SELECT 
 	year, 
 	ROUND (AVG (value), 1) AS avg_price_in_CZK,
-	CONCAT (category, ' ', price_value, ' ', price_unit) AS category_and_unit,
+	category, 
+	price_value,
+	price_unit,
 	ROUND (AVG (value_payroll)) AS avg_salary_in_CZK,
-	CONCAT (FLOOR (AVG (value_payroll)/AVG (value)), ' ', price_unit) AS affordability_index_and_unit -- zaokrouhluji dolu, na cele kusy zbozi 
+	FLOOR (AVG (value_payroll)/AVG (value)) AS affordability_index,
+	price_unit 
 FROM t_veronika_polachova_project_SQL_primary_final tvppspf 
 WHERE year IN (2006, 2018)
 AND category IN ('Chléb konzumní kmínový', 'Mléko polotučné pasterované')
-AND value_type_code = 5958
-GROUP BY year, category,price_unit, price_value 
+GROUP BY year, category, price_unit, price_value 
 ORDER BY `year` 
 ;
