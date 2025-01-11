a1773eeb7c3e7008d84e276b7abceea36450c743
VeronikaPolachova <veronika.cunderlova@gmail.com>
2025-01-11 17:51:21 +0100

    GDP perc. growth

--- otazka5.sql
+++ otazka5.sql
@@ -9,7 +9,10 @@ SELECT
  	ROUND (AVG (p.value), 1) AS avg_price,
  	ROUND (LAG (AVG (p.value)) OVER (ORDER BY p.payroll_year), 1) AS previous_year_price,
 	s.GDP,
-	LAG (s.GDP) OVER (ORDER BY s.`year`) AS previous_year_GDP
+	LAG (s.GDP) OVER (ORDER BY s.`year`) AS previous_year_GDP,
+	ROUND ((((AVG (p.value_payroll)) - (LAG (AVG (p.value_payroll)) OVER (order by p.payroll_year)))/(LAG (AVG (p.value_payroll)) OVER (order by p.payroll_year))*100), 2) AS percentage_salary_growth,
+	ROUND ((((AVG (p.value)) - (LAG (AVG (p.value)) OVER (order by p.payroll_year)))/(LAG (AVG (p.value)) OVER (order by p.payroll_year))*100), 2) AS percentage_price_growth,
+	ROUND (((s.GDP - (LAG (s.GDP) OVER (ORDER BY s.`year`)))/(LAG (s.GDP) OVER (ORDER BY s.`year`)))*100, 2) AS percentage_GDP_growth 
 FROM t_veronika_polachova_project_SQL_primary_final p
 LEFT JOIN t_veronika_polachova_project_SQL_secondary_final s
 ON p.payroll_year = s.`year` 
