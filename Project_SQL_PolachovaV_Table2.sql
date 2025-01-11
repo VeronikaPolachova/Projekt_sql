7c408d61c69863934c6694e2c4b7d8248b7f8034
VeronikaPolachova <veronika.cunderlova@gmail.com>
2025-01-11 14:48:30 +0100

    create table

--- Project_SQL_PolachovaV_Table2.sql
+++ Project_SQL_PolachovaV_Table2.sql
@@ -1,8 +1,7 @@
 -- Tabulka 2
-
+CREATE OR REPLACE TABLE t_veronika_polachova_project_SQL_secondary_final AS
 SELECT 
 	c.country, 
-	c.continent,
 	e.year,
 	e.population,
 	ROUND (e.GDP) AS GDP,
@@ -13,4 +12,8 @@ ON c.country = e.country
 WHERE e.year BETWEEN 2006 AND 2018
 AND c.continent = 'Europe'
 ORDER BY country, year
+;
+
+SELECT *
+FROM t_veronika_polachova_project_SQL_secondary_final tvppssf 
 ;
